2908eaf137b6a2385ed41f2fc565c42e305bdd91
lethalaric <mukhtarharis.lethalaric@gmail.com>
2015-03-10 04:23:34 +0700

    Fitness-Proportionate Selection
    Genetic programming - FPS

---
+++ Fitness proportionate selection.py
@@ -0,0 +1,20 @@
+from random import randint
+import collections
+
+ind = collections.namedtuple('individu', 'ind')
+
+def __init__(self, pop_total = 10)
+	self.pop_total = pop_total
+
+def fps (a)
+	total = sum(a)	
+	b = randint(0, total)
+	awal = 0
+	for x in a.item():
+		awal += x
+		if awal > b:
+			return x			
+			
+"""
+masih ga jelas
+"""
